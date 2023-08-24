21d51bc8b81cb69b819273ccbfb4dec01b99f17f
LunaticAnt <alwlqkrtk@naver.com>
2023-08-24 17:55:32 +0900

    엄....

--- db/database.py
+++ db/database.py
@@ -14,13 +14,15 @@ def create_tables():
     """Create tables in the database."""
     Base.metadata.create_all(bind=engine)
 
-# def ensure_tables_exist():
-#     for table in tableList:
-#         if not SuchTable(table.__tablename__):
-#             table.__table__.create(bind=engine, checkfirst=True)
+'''
+def ensure_tables_exist():
+    for table in tableList:
+        if not SuchTable(table.__tablename__):
+            table.__table__.create(bind=engine, checkfirst=True)
 
-# def SuchTable(table_Name):
-#     inspector = inspect(engine)
-#     if table_Name in inspector.get_table_names():
-#         return True
-#     return False
+def SuchTable(table_Name):
+    inspector = inspect(engine)
+    if table_Name in inspector.get_table_names():
+        return True
+    return False
+'''
