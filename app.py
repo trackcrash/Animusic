843990c3dfecb39f7810e64ecbfe0e85331fa04a
trackcrash <auroknh@gmail.com>
2023-08-30 23:08:54 +0900

    refactoring

--- app.py
+++ app.py
@@ -2,32 +2,27 @@
 from decouple import config
 from flask import Flask,render_template, request, redirect, url_for, jsonify, flash, send_file
 from flask_login import LoginManager, current_user, login_required, logout_user
-from chat.chat import socketio, get_room_dict, get_user
-from chat.chat_model import make_answer
-from controllers import play_controller, login_controller
-from models import login_model
+from view.play import play_bp
+from view.map import map_bp
+from view.user import login_bp
+from controllers.play_controller import get_room_dict, get_user
+from controllers import map_controller, login_controller
+from models.user_model import get_user_by_id, delete_account, account_insert, account_insert_in_googleuser, insert_character_number
+from models.play_model import make_answer
 import requests, os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config('SECRET_KEY')
 
+app.register_blueprint(play_bp)
+app.register_blueprint(map_bp)
+app.register_blueprint(login_bp)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-socketio.init_app(app,cors_allowed_origins="*")
-
-@app.get('/get-music-data')
-def get_music_data():
-    mission_id = request.args.get('id')
-    return play_controller.single_make_answer(mission_id)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return login_model.get_user_by_id(user_id)
-
-@app.route('/single_list')
-def single_select():
-    missions = play_controller.show_mission()
-    return render_template('single_select.html', current_user=current_user,missions=missions)
+    return get_user_by_id(user_id)
 
 @app.route('/single-play', methods=['GET', 'POST'])
 def single_play():
@@ -37,103 +32,8 @@ def single_play():
 def index():
     return render_template('index.html', current_user=current_user)
 
-@app.route('/play', methods=['GET', 'POST'])
-def play():
-    return play_controller.play_controller()
-
-@app.route('/Show')
-def show():
-    return play_controller.show_table()
-
-@app.route('/submit-to-db', methods=['POST'])
-def submit():
-    return play_controller.submit_to_db()
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-
-    if request.method == 'POST':
-        login_successful = login_controller.user_controller()
-        if login_successful:
-            return redirect(url_for('index'))
-
-        flash('Invalid email or password')
-        return redirect(url_for('login'))
-
-
-#인증 관련 부분
-@app.get('/login/google')
-def google_login():
-    return login_controller.google_login()
-
-
-@app.get('/login/google/callback')
-def google_callback():
-    return login_controller.google_callback()
-
-
-@app.get('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        return login_controller.register()
-    return render_template('register.html')
 
 #----------회원 탈퇴 & 회원 정보수정-----------
-@app.get('/delete_account_confirm')
-def delete_account_confirm():
-    return render_template('account_management/deleteAccountConfirm.html')
-
-@app.get('/delete_account')
-def deleting_account():
-    print(f"\033[92;1m{current_user.name}님의 회원탈퇴 시도중...\033[0m")
-    result_delete_account = login_model.delete_account()
-    if result_delete_account:
-        print("\033[92;1m회원 탈퇴처리 완료\033[0m")
-        return render_template('account_management/deleteAccountComplete.html')
-    else:
-        print("\033[92;1m회원 탈퇴처리 실패\033[0m")
-        return render_template('account_management/deleteAccountFail.html')
-@app.get('/account_confirm')
-def account_confirm():
-    print("체크 중: ", current_user.is_authenticated)
-    if current_user.is_authenticated == False:
-        return render_template('account_management/accountConfirm.html')
-    else:
-        return render_template('account_management/managementAccountPage.html')
-@app.route('/get_current_user_is_google_authenticated', methods=['GET'])
-def get_current_user_is_google_authenticated():
-    return jsonify({'check_googleuser': current_user.is_google_authenticated})
-
-@app.route('/update_profile', methods=['POST'])
-def update_profile():
-    data = request.json
-    nickname = data[0]['nickname']
-    password = data[0]['password']
-    newpassword = data[0]['newpassword']
-
-    if current_user.is_google_authenticated: # 구글 유저 인 경우
-        if login_model.account_insert_in_googleuser(nickname):
-            print(f"\033[92;1m사용자의 닉네임이 {nickname}으로 변경되었습니다.\033[0m")
-            return jsonify({'message': '변경이 완료되었습니다.'}), 200
-        else:
-            print("\033[92;1m이미 존재하는 닉네임을 가진 회원이 있으므로 변경할 수 없습니다.\033[0m")
-            return jsonify({'message': '이미 동일한 닉네임이 존재합니다.'}), 400
-    else:
-        if login_model.account_insert(nickname, password, newpassword):
-            print("\033[92;1m사용자의 회원정보가 변경되었습니다.\033[0m")
-            return jsonify({'message': '변경이 완료되었습니다.'}), 200
-        else:
-            print("\033[92;1m사용자의 회원정보 변경이 실패하였습니다.\033[0m")
-            return jsonify({'message': '기존 비밀번호가 일치하지 않거나 동일한 닉네임이 존재합니다.'}), 400
 
 # 캐릭터 변경기능
 @app.route('/select_character')
@@ -145,24 +45,24 @@ def select_character():
 @app.route('/insert_character', methods=['POST'])
 def insert_character():
     character_number = request.get_json().get('character_number')
-    return login_model.insert_character_number(character_number)
+    return insert_character_number(character_number)
 # -------------------------------------
 ####################################################################################
 
 #미션 관리부분
 @app.route('/Map')
 def mymap():
-    data_list = play_controller.show_mission_byProducer()
+    data_list = map_controller.show_mission_byProducer()
     return render_template('Map.html', data_list=data_list)
 
 @app.post('/update-to-db')
 def update():
-    return play_controller.update_to_db()
+    return map_controller.update_to_db()
 
 
 @app.get("/delete-mission")
 def deleteMission():
-    return play_controller.delete_Mission(request.args.get('id'))
+    return map_controller.delete_Mission(request.args.get('id'))
 
 @app.route('/download_excelfile', methods=['GET'])
 def download_template():
@@ -224,7 +124,7 @@ def chat_post():
 
 @app.get('/api/get_mission_table')
 def get_mission_table():
-    mission_table_data = play_controller.show_mission_active()
+    mission_table_data = map_controller.show_mission_active()
     return jsonify(mission_table_data)
 
 @app.route('/robots.txt')
@@ -232,6 +132,5 @@ def robots():
     return send_file('robots.txt')
 
 if __name__ == '__main__':
-    play_controller.ensure_tables_exist()
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
-
+    map_controller.ensure_tables_exist()
+    app.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
---
+++ models/play_model.py
@@ -0,0 +1,169 @@
+import random
+from controllers.play_controller import show_table_bymissionid
+from models.user_model import get_user_by_id
+
+class RoomDataManger:
+    def __init__(self):
+        self._data_store = dict()
+    def remove_room(self, room_name):
+        if room_name in self._data_store:
+            del self._data_store[room_name]
+    def user_left(self, room_name, user_id):
+        if room_name in self._data_store and "user" in self._data_store[room_name]:
+            users = self._data_store[room_name]["user"]
+        
+            
+        if len(users) > 0:
+            if user_id in users and users[user_id]["host"] == 1:
+            # 가장 오랜 시간 동안 머문 사용자를 새로운 방장으로 설정
+                longest_present_user = max(users, key=lambda user: users[user]["joined_time"])
+                users[longest_present_user]["host"] = 1
+                return longest_present_user
+    def room_check(self, room_name):
+        if room_name in self._data_store:
+            return True
+        else:
+            return False
+    def create_room(self,room_name,session_id):
+            if room_name in self._data_store:
+                return False
+            
+            # 방 중복생성 금지 (클라이언트에 해당 이벤트 요청)
+            #방을 생성할 사용자의 정보를 room_data_manager에 저장
+            #해당 사용자의 세션 id
+            print(f"해당 사용자의 방 생성 정보: {session_id, room_name}")
+            #방 정보 room_data_manager에 담기 위한 data
+            room_data = {
+                "room_info":{
+                    "session_id":session_id,
+                    #room_password, room_status, playing\
+                    "room_status" : False
+                },
+                "user":
+                {
+
+                }
+            }
+            dict_create(self._data_store,room_name,room_data)
+            print(f"{room_name}님이 방을 생성하셨습니다.")
+            return True
+    def join(self, room_name, session_id, current_user,time):
+        user_name = current_user.name
+        character = get_user_by_id(current_user.id).character
+        character_link = "/static/img/character/" + str(character) + ".png"
+        print(f"{room_name}방에 연결되었습니다.")
+        user_data = {'username': user_name , 'host':0 ,'score':0 ,'joined_time' : time.time(), 'character':character_link}  # 유저 데이터를 리스트로 생성
+        dict_join(self._data_store[room_name]["user"], session_id, user_data)
+    def host_setting(self,room_name, callback=None):  
+        if room_name in self._data_store and "user" in self._data_store[room_name]:
+            users = self._data_store[room_name]["user"]
+            if users:  # 유저 정보가 있는 경우
+                first_user_key = next(iter(users))  # 첫 번째 유저의 키를 가져옵니다.
+                if first_user_key in users:
+                    users[first_user_key]["host"] = 1  # host를 1로 변경합니다.
+                    # 변경된 정보 출력
+                    print("First user's data after modification:", users[first_user_key])
+                    return first_user_key
+                else:
+                    print("First user not found in the user dictionary.")
+            else:
+                print("No user data in the room.")
+                return ""
+        else:
+            print("Room not found or no user data in the room.")
+            return ""
+        if callback:
+            callback()  # 콜백 함수 호출
+    def is_user_in_room(self, user_name, room_name):
+        dictionaryData = self._data_store[room_name]['user']
+        for key, value in dictionaryData.items():
+            if 'username' in value and value['username'] == user_name:
+                return True
+        return False
+    def game_status(self, room_name, booldata):
+        self._data_store[room_name]['room_info']['room_status'] = booldata
+    def game_init(self, room_name):
+        dictionaryData = self._data_store[room_name]['user']
+        for key, value in dictionaryData.items():
+            value['score'] = 0
+
+room_data_manager = RoomDataManger()
+class MusicDataManager:
+    def __init__(self):
+        self._data_store = dict()
+
+    def store_data(self, room_name, data):
+        self._data_store[room_name] = {
+            'data': data,
+            'current_index': 0
+        }
+    def index_data(self, room_name):
+        return self._data_store.get(room_name, {}).get('current_index', 0)
+    
+    def retrieve_data(self, room_name):
+        index = self._data_store.get(room_name, {}).get('current_index', 0)
+        return self._data_store.get(room_name, {}).get('data', [])[index]
+
+    def retrieve_next_data(self, room_name):
+        room_data = self._data_store.get(room_name, {})
+        room_data['current_index'] += 1
+        data = room_data.get('data', [])
+        idx = room_data.get('current_index', 0)
+        if idx < len(data):
+            next_item = data[idx]
+            return next_item
+        return None
+
+    def remove_data(self, room_name):
+        if room_name in self._data_store:
+            del self._data_store[room_name]
+    
+    def check_answer(self, room_name, answer):
+        room_data = self._data_store.get(room_name, {})
+        data = room_data.get('data', [])
+        idx = room_data.get('current_index', 0)
+        # Data is available
+        if idx < len(data):
+            next_item = data[idx]
+            if answer in next_item['answer_list']:
+                return True
+        return False
+
+music_data_manager = MusicDataManager()
+
+def make_answer(mission_id, room_name):
+    data = show_table_bymissionid(mission_id)
+    result = []
+
+    for item in data:
+        youtube_embed_url = f"https://www.youtube.com/embed/{item['youtube_url'].split('=')[-1]}?autoplay=1"
+        answer_list = [answer.strip() for answer in item['answer'].split(',')]
+        starttime = float(item['startTime'])
+        endTime = float(item['endTime'])
+        music_data = {
+            'hint': item['hint'],
+            'startTime': starttime,
+            'endTime' : endTime ,
+            'is_answered': 'false',
+            'answer_list': answer_list,
+            'youtube_embed_url': youtube_embed_url,
+            'title': item['title'],
+            'song': item['song']
+        }
+        result.append(music_data)
+
+    random.shuffle(result)
+
+    music_data_manager.store_data(room_name, result)
+    return room_name
+
+
+
+def dict_join(dict_name,dict_index,dict_value):
+    if dict_index in dict_name:
+        dict_name[dict_index].update(dict_value)
+    else :
+        dict_name[dict_index] = dict_value
+
+def dict_create(dict_name,dict_index,dict_value):
+        dict_name[dict_index] = dict_value
