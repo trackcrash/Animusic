5fa08438acfd37db22e8795536e384e8fe7cd8ee
trackcrash <auroknh@gmail.com>
2023-08-30 23:49:23 +0900

    refactoring

--- view/map.py
+++ view/map.py
@@ -1,10 +1,8 @@
-import requests
-from decouple import config
 from flask import Blueprint, render_template, request, jsonify, send_file
 from controllers.map_controller import show_mission, show_table,single_make_answer,submit_to_db,show_mission_byProducer,update_to_db,delete_Mission
 from flask_login import current_user
-from controllers import map_controller
-map_bp = Blueprint('map', __name__, url_prefix='/map')
+from controllers.map_controller import map_controller, videoid_check, show_mission_active
+map_bp = Blueprint('map', __name__)
 
 @map_bp.get('/get-music-data')
 def get_music_data():
@@ -50,18 +48,10 @@ def download_template():
 #재생가능한 비디오링크인지 체크해주는 기능(요청 1번에 최대 42개의 비디오링크까지 가능)
 def check_videoid():
     videoid_list, convert_list = set(request.get_json()), set()
-    result = videoid_list.copy()
-    for id_count, videoid in enumerate(videoid_list):
-        convert_list.add("&id=" + videoid)
-        if (id_count + 1) % 42 == 0:
-            request_text = "".join(map(str, convert_list))
-            api_result = requests.get(f'https://www.googleapis.com/youtube/v3/videos?key={config("YOUTUBE_API_KEY")}&part=status{request_text}')
-            renewable_list = set(item['id'] for item in api_result.json()['items'] if item['status']['embeddable'])
-            result -= renewable_list
-            convert_list.clear()
-    if len(convert_list) > 0:
-        request_text = "".join(map(str, convert_list))
-        api_result = requests.get(f'https://www.googleapis.com/youtube/v3/videos?key={config("YOUTUBE_API_KEY")}&part=status{request_text}')
-        renewable_list = set(item['id'] for item in api_result.json()['items'] if item['status']['embeddable'])
-        result -= renewable_list
+    result = videoid_check(videoid_list, convert_list)
     return jsonify(list(result))
+
+@map_bp.get('/api/get_mission_table')
+def get_mission_table():
+    mission_table_data = show_mission_active()
+    return jsonify(mission_table_data)
