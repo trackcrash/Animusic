15f349218d53452139961f53817bd59aa5031372
trackcrash <auroknh@gmail.com>
2023-08-31 03:20:27 +0900

    code clean up

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 #flask main --author: NewKyaru 11/08/2023
+#library
 from decouple import config
 from flask import Flask,render_template, send_file
 from flask_login import LoginManager, current_user
-from controllers import map_controller
+#controller&model&view(blueprint)
+from controllers.map_controller import ensure_tables_exist
 from models.user_model import get_user_by_id
 from view.play import play_bp
 from view.map import map_bp
 from view.user import user_bp
 from view.room import room_bp
 from view.character import char_bp
+#socket
 from Socket.socket import socketio
-
+from Socket.connect_socket import connect_MySocket
+from Socket.play_socket import play_Socket
+from Socket.room_socket import room_Socket
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config('SECRET_KEY')
 
@@ -21,11 +26,9 @@ app.register_blueprint(room_bp)
 app.register_blueprint(char_bp)
 socketio.init_app(app)
 
-from Socket.connect_socket import connect_MySocket
+
 connect_MySocket(socketio)
-from Socket.play_socket import play_Socket
 play_Socket(socketio)
-from Socket.room_socket import room_Socket
 room_Socket(socketio)
 
 
@@ -50,5 +53,5 @@ def sitemap():
     return send_file('sitemap.xml')
 
 if __name__ == '__main__':
-    map_controller.ensure_tables_exist()
+    ensure_tables_exist()
     socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
