d5144f09d64db7db5ea944541ffe61bdf40f96d6
LunaticAnt <alwlqkrtk@naver.com>
2023-09-04 22:08:32 +0900

    mapcreate tutorial

--- static/js/map_tutorial.js
+++ static/js/map_tutorial.js
@@ -30,30 +30,30 @@ function manual_make() {
             <button id="tutorial-close" type="button" class="px-2 py-1 mt-1 ml-2 bg-red-500 text-white rounded hover:bg-red-600">&times;</button>
         </div>
         <div class="mt-10 y-full">
-            <span style="font-size: 20px">1. 맵 이름</span>
+            <span style="font-size: 24px">1. 맵 이름</span>
         </div>
         <div class="mt-2 y-full tutorial-font">
             <p><span>맵 이름을 적는 입력 칸은 입력 후</span>
-                <span class="px-1 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">제목 저장하기</span>
+                <span class="p-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">저장하기</span>
                 <span>를 누르면</span></p>
             <p><span>맵을 다 만들고 하단의</span>
-                <span class="px-1 py-1 bg-green-500 text-white rounded hover:bg-green-600 transition">저장하기</span>
-                <span>버튼을 누르기 전 까지</span></p>
-            <p><span>건드릴 일이 없습니다.</span></p>
+                <span class="p-1 bg-green-500 text-white rounded hover:bg-green-600 transition">저장하기</span>
+                <span>또는</span>
+                <span class="p-1 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition">정보 수정하기</span></p>
+            <p><span>버튼을 누르기 전 까지</span>
+                <span>건드릴 일이 없습니다.</span></p>
             <p><span>혹시 맵 이름을 중간에 바꾸고 싶으시다면</span>
-                <span class="px-1 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">제목 변경하기</span>
-            <p><span>또는</span>
-                <span class="px-1 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition">정보 수정하기</span>
+                <span class="p-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">제목 변경하기</span>
                 <span>를 누르고</span></p>
             <p><span>바꾸시고 싶은 이름으로 변경 후 다시</span>
-                <span class="px-1 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">제목 저장하기</span>
+                <span class="p-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">저장하기</span>
                 <span>를 누르시면 됩니다.</span></p>
         </div>
-        <div class="mt-10 y-full px-2 py-2 border border-solid border-black rounded bg-gray-100">
+        <div class="mt-10 y-full p-2 border border-solid border-black rounded bg-gray-100">
             <label id="tutorial-mapname-label">맵 이름: </label>
             <div class="w-full flex">
-                <input id="tutorial-mapname-input" class="w-1/2 py-2 mx-1 border border-gray-300 rounded" type="text">
-                <button id="tutorial-mapname-insert" type="button" class="w-1/2 py-2 mx-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">제목 저장하기</button>
+                <input id="tutorial-mapname-input" class="w-1/2 p-2 mx-1 border border-gray-300 rounded" type="text">
+                <button id="tutorial-mapname-insert" type="button" class="w-1/2 py-2 mx-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition">저장하기</button>
             </div>
         </div>
         <div class="flex flex-col">
@@ -64,17 +64,62 @@ function manual_make() {
                 <button id="tutorial-mapname-insertbtn" type="button" class="w-1/2 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition">정보 수정하기</button>
             </div>
         </div>
-        <div class="mt-10 y-full px-2 py-2 border border-solid border-black rounded bg-gray-100" style="font-size: 18px;">
+        <div class="flex flex-col mt-10 y-full px-2 py-2 border border-solid border-black rounded bg-gray-100" style="font-size: 18px;">
             <label>실제 적용되는 맵 이름</label>
             <label id="tutorial-mapname"></label>
         </div>
-
+        <div class="flex">
+            <div class="w-1/2"></div>
+            <div class=" w-1/2 flex mt-10">
+                <button type="button" onclick="open_tutorial()" class="py-1 mx-1 w-1/2 bg-red-300 text-white rounded hover:bg-red-400 transition">&#x25C0; 이전페이지</button>
+                <button type="button" onclick="manual2_make()" class="py-1 mx-1 w-1/2 bg-red-300 text-white rounded hover:bg-red-400 transition">다음페이지 &#x25B6;</button>
+            </div>
+        </div>
     `;
     document.getElementById('tutorial-close').addEventListener('click', () => {close_tutorial()});
     document.getElementById('return-tutorial').addEventListener('click', () => {open_tutorial()});
+    const tutorial_mapname_label = document.getElementById('tutorial-mapname-label');
+    const tutorial_mapname_input = document.getElementById('tutorial-mapname-input');
+    const tutorial_mapname_insert = document.getElementById('tutorial-mapname-insert');
+    tutorial_mapname_insert.addEventListener('click', () => {
+        if (tutorial_mapname_input.classList.contains('hidden')) {
+            tutorial_mapname_insert.classList.remove('w-full');
+            tutorial_mapname_insert.classList.add('w-1/2');
+            tutorial_mapname_input.classList.remove('hidden');
+            tutorial_mapname_insert.innerText = '저장하기';
+        } else {
+            tutorial_mapname_label.textContent = "맵 이름: " + tutorial_mapname_input.value;
+            tutorial_mapname_input.classList.add('hidden');
+            tutorial_mapname_insert.classList.remove('w-1/2');
+            tutorial_mapname_insert.classList.add('w-full');
+            tutorial_mapname_insert.innerText = '제목 변경하기';
+        };
+    });
+    const tutorial_mapname = document.getElementById('tutorial-mapname');
+    document.getElementById('tutorial-mapname-savebtn').addEventListener('click', () => {
+        tutorial_mapname.textContent = tutorial_mapname_input.value;
+    });
+    document.getElementById('tutorial-mapname-insertbtn').addEventListener('click', () => {
+        tutorial_mapname.textContent = tutorial_mapname_input.value;
+    });
+};
 
+function manual2_make() {
+    tutorial_Content.innerHTML =`
+        <div class="flex items-center justify-end">
+            <button id="return-tutorial" type="button" class="px-2 py-1 mt-1 bg-blue-500 text-white rounded hover:bg-blue-600">처음으로 돌아가기</button>
+            <button id="tutorial-close" type="button" class="px-2 py-1 mt-1 ml-2 bg-red-500 text-white rounded hover:bg-red-600">&times;</button>
+        </div>
+        <div class="mt-10 y-full">
+            <span style="font-size: 24px">2. 제목, 곡 이름</span>
+        </div>
+    `;
+    document.getElementById('tutorial-close').addEventListener('click', () => {close_tutorial()});
+    document.getElementById('return-tutorial').addEventListener('click', () => {open_tutorial()});
 }
 
+
+//-------------------------------액셀파일로 만드는 방법 튜토리얼 부분---------------------------//
 function auto_make() {
     tutorial_Content.innerHTML = `
         <div class="flex items-center justify-end">
