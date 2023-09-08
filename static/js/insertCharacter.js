625934bf32e7e8902366418b1440a97e77703cd6
trackcrash <auroknh@gmail.com>
2023-09-08 20:08:33 +0900

    design changed

--- static/js/insertCharacter.js
+++ static/js/insertCharacter.js
@@ -2,22 +2,22 @@ function sendImageIdToServer(id) {
     const url = '/insert_character';
 
     fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ character_number: id })
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert('변경되었습니다.')
-        window.location.href='/';
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('변경에 실패하였습니다.')
-        location.reload();
-    });
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ character_number: id })
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert('변경되었습니다.')
+            window.location.href = '/';
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('변경에 실패하였습니다.')
+            location.reload();
+        });
 };
 /* <li>
 <img id="{{ character.split('.')[0] }}" src="{{ url_for('static', filename='img/character/' + character) }}" alt="캐릭터 이미지" class="clickable-image block bg-white p-6 shadow-lg hover:shadow-xl hover:bg-gray-200 rounded transition duration-300">
@@ -37,8 +37,8 @@ window.onload = () => {
 
         // 클래스를 추가합니다.
         img.classList.add(
-            "clickable-image", "block", "bg-white", "p-6", 
-            "shadow-lg", "hover:shadow-xl", "hover:bg-gray-200", 
+            "clickable-image", "block", "bg-gray-500",
+            "shadow-lg", "hover:shadow-xl", "hover:bg-gray-400",
             "rounded", "transition", "duration-300"
         );
 
@@ -47,9 +47,18 @@ window.onload = () => {
 
         list.appendChild(img);
         CharacterSelect.appendChild(list);
-        img.addEventListener("click", () =>
-        {
+        img.addEventListener("click", () => {
             sendImageIdToServer(i);
         })
     }
 }
+
+img.addEventListener("click", () => {
+    img.style.transition = "transform 0.3s";
+    img.style.transform = "scale(0.9)";
+    setTimeout(() => {
+        img.style.transform = "scale(1)";
+    }, 300);
+
+    sendImageIdToServer(i);
+});
