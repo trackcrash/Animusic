1fb8d6cb07ec1c7997cf5a2f054fe6b2e93607f2
trackcrash <auroknh@gmail.com>
2023-09-13 20:57:53 +0900

    event change

--- static/js/notify.js
+++ static/js/notify.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
     const notificationIcon = $("#notificationIcon");
     const dropdown = $("#notificationList");
     const notificationCountElem = $("#notificationCount");
