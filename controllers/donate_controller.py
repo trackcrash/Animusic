cc108993933106de3830711a1842b1d54bde21ff
trackcrash <auroknh@gmail.com>
2023-09-26 06:43:12 +0900

    update logic

--- controllers/donate_controller.py
+++ controllers/donate_controller.py
@@ -1,4 +1,4 @@
-import json,requests
+import hmac, hashlib,decouple
 from decouple import config
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import login_user,current_user
@@ -7,21 +7,22 @@ from oauthlib.oauth2 import WebApplicationClient
 from models import donate_model
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
+coffeecode = config('BUY_COFFEE_KEY')
+
 def donation(amount):
     try:
-        # 금액을 이용한 실제 입금 처리 로직을 여기에 추가합니다.
-        # 이 예시에서는 로그에 금액을 출력하는 것으로 대체합니다.
         donate_model.save_donation(amount)
-        
         print(f'금액 {amount} 원이 입금되었습니다.')
         
-        # 실제로는 여기에서 결제 처리 서비스와 연동하여 금액을 처리해야 합니다.
-        
-        return True  # 입금 처리가 성공적으로 완료됐을 경우 True 반환
+        return True
     except Exception as e:
-        # 오류가 발생하면 오류 메시지 출력
         print(f'입금 처리 중 오류 발생: {str(e)}')
-        return False  # 입금 처리가 실패한 경우 False 반환
+        return False
+    
+def verify_signature(request_body, received_signature):
+    computed_signature = hmac.new(bytes(coffeecode , 'latin-1'), msg = request_body , digestmod = hashlib.sha256).hexdigest()
+    return hmac.compare_digest(computed_signature, received_signature)
 
 def donate_del():
     donate_model.donation_delete()
--- view/donate.py
+++ view/donate.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify,redirect
 from flask_login import current_user
-from controllers.donate_controller import donation
+from controllers.donate_controller import donation, verify_signature
 donate_bp = Blueprint('donate', __name__, url_prefix='')
 
 @donate_bp.get('/donate')
@@ -9,16 +9,25 @@ def donate_page():
         return render_template('donate.html')
     else:
         return redirect('/')
+    
 @donate_bp.route('/api/donate', methods=['POST'])
 def donate_api():
+    #received_signature = request.headers.get('X-Signature')
+    request_body = request.data
+
+    # if not verify_signature(request_body, received_signature):
+    #     return jsonify({"message": "Invalid signature"}), 400
+
     data = request.json
-    amount = float(data.get('amount'))
-    # donation 함수를 호출하여 후원 금액을 처리합니다.
-    donation_result = donation(amount)
+    inner_data = data.get('data', {})
+    amount = float(inner_data.get('amount', 0))
     
+    donation_result = donation(amount)
+
     if donation_result:
         response_data = {'message': '후원이 성공적으로 처리되었습니다.'}
         return jsonify(response_data), 200
     else:
         response_data = {'message': '후원 처리 중 오류가 발생했습니다.'}
         return jsonify(response_data), 500
+
